d2e7e1a3ed97aeb7ba52da809846511fb1891c12
Aug <aug@example.com>
2024-08-23 01:09:41 +0000

    测试

--- exp/tt_asr/cli.py
+++ exp/tt_asr/cli.py
@@ -1,5 +1,6 @@
 import requests
-
+import time
+s_time = time.time()
 input_audio = "/mnt/user2/workspace/Aug/code/Chat/exp/VoiceStreamAI/zhang-7-0.mp3"
 with open(input_audio, 'rb') as f:
     response = requests.post(
@@ -7,4 +8,6 @@ with open(input_audio, 'rb') as f:
         files={'file': ('output.mp3', f, 'audio/mp3')})
 data = response.json()  # Get the JSON response
 text = data['transcription'][1]['text']
+e_time = time.time()
+print("识别时间：", e_time - s_time)
 print("识别结果：", text)
